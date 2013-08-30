d105c0d824045ac139ed0c1c5fbf706a4e5f2a5f
okatsuralau <gabrielslau@yahoo.com.br>
2013-08-30 17:38:49 -0300

    implementação da classe Banco.

---
+++ src/br/ifrn/tads/poo/banco/Banco.java
@@ -0,0 +1,41 @@
+package br.ifrn.tads.poo.banco;
+
+import br.ifrn.tads.poo.banco.agencia.Agencia;
+import java.util.ArrayList;
+
+public class Banco {
+
+  private String nome;
+  private int numero;
+  private ArrayList<Agencia> agencias; // lista das agencias cadastradas no banco
+
+  public Banco(int numero, String nome) {
+	  this.numero = numero;
+	  this.nome = nome;
+	  this.agencias = new ArrayList<Agencia>();
+  }
+
+  public String getNome() {
+	  return nome;
+  }
+
+  public int getNumero() {
+	  return numero;
+  }
+
+  public Agencia buscarAgencia(int numero) {
+	  Agencia out = null;
+	  for(Agencia agencia: this.agencias){
+          if( numero == agencia.numero ){
+        	  out = agencia;
+        	  break;
+          }
+      }
+	  return out;
+  }
+
+  public void adicionarAgencia(Agencia agencia) {
+	  this.agencias.add(agencia);
+  }
+
+}
